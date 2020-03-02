803fba1878dadc8cf98ad572ec59570dbddad64c
gilangrkun <gilang.grk17@gmail.com>
2020-03-02 15:00:32 +0700

    menambahkan direktori novice/01-01/latihan/ dan file latihan

---
+++ novice/01-01/latihan/StrukturDasar.java
@@ -0,0 +1,7 @@
+public class StrukturDasar{ /*nama dari suatu program*/
+
+    /*di bawah ini merupakan method*/
+    public static void main(String[] args) {
+        System.out.println("Struktur dasar source code dalam bahasa pemrograman Java");
+    }
+}
